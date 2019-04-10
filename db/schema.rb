4c74d9b19b65ad4d2e57c9ae9c4e249c9b293fb5
Dave Gonzalez <dvgz@outlook.com>
2019-04-09 20:21:32 -0400

    rolled back migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_222702) do
+ActiveRecord::Schema.define(version: 2019_03_30_235309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_04_09_222702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "done"
-    t.datetime "finishByDate"
   end
 
   add_foreign_key "items", "todos"
