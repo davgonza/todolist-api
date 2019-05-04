6865d1f6b24ff126cd3c9b43e9defb9526f9633c
Dave Gonzalez <dvgz@outlook.com>
2019-05-04 17:58:29 -0400

    added jwt and bcrypt to gemfile; also generated schema, getting ready to authenticate user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_201453) do
+ActiveRecord::Schema.define(version: 2019_05_04_213213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2019_05_01_201453) do
     t.boolean "done"
     t.datetime "finish_by_date"
     t.boolean "priority"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "todos"
