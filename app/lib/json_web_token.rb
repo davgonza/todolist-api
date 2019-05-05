5fed3ead170178b99722af4aaebc5ea9fbf287f9
Dave Gonzalez <dvgz@outlook.com>
2019-05-05 17:52:22 -0400

    test

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,13 +1,11 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
-
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, TodosApi::Application.credentials.secret_key_base)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, TodosApi::Application.credentials.secret_key_base)[0]
     HashWithIndifferentAccess.new body
   rescue JWT::DecodeError => e
     raise ExceptionHandler::InvalidToken, e.message
